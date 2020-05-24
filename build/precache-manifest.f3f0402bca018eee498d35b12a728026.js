cadeb35502f1f1727000e27a203e7cfbe389688e
Shreyash Nandgaonkar <46418251+ShreyashNandgaonkar@users.noreply.github.com>
2020-05-24 18:59:16 +0530

    Add files via upload

---
+++ build/precache-manifest.f3f0402bca018eee498d35b12a728026.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74b8d640aeeb69bf59bbd94e6557a2e1",
+    "url": "/recipeApp-redux/index.html"
+  },
+  {
+    "revision": "d3a2c2add24c3b1f1ced",
+    "url": "/recipeApp-redux/static/css/main.30752d5e.chunk.css"
+  },
+  {
+    "revision": "233e2a67e3964aaf70f3",
+    "url": "/recipeApp-redux/static/js/2.db1f8555.chunk.js"
+  },
+  {
+    "revision": "d3a2c2add24c3b1f1ced",
+    "url": "/recipeApp-redux/static/js/main.8399e791.chunk.js"
+  },
+  {
+    "revision": "e8ca13cf83ddad80819d",
+    "url": "/recipeApp-redux/static/js/runtime~main.ac42dab7.js"
+  }
+]);
